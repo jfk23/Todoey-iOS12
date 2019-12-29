865f4c03ee7ebdbb44a9d6c9fc3a21af458b1b76
Byungsuk Choi <jfk23@msn.com>
2019-12-29 15:39:03 -0500

    Initial Commit

---
+++ Todoey-iOS12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey-iOS12
+//
+//  Created by Byungsuk Choi on 12/29/19.
+//  Copyright © 2019 Byungsuk Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
